a5fa169ef1ab673a5eea92d3b89c20dfbb9aedc6
desainkr <desainkr@gmail.com>
2026-02-09 17:52:41 -0800

    login file updated

--- tests/IntegrationTesting/login.spec.ts
+++ tests/IntegrationTesting/login.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../Pages/LoginPage';
 import { BASE_URL, USERNAME, PASSWORD } from '../utils/envConfig'
-test('Login to saucedemo application with valid crdentials', async ({ page }) => {
+test('Login to saucedemo application with valid credentials', async ({ page }) => {
     const loginpage = new LoginPage(page);
     await page.goto(BASE_URL);
     await loginpage.login(USERNAME, PASSWORD);
     await expect(page.getByText('Swag Labs', { exact: true })).toBeVisible();
     //await expect(page).toHaveURL(/inventory/);
-    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    await expect(page).toHaveURL(/inventory.html/);
     await expect(page).toHaveTitle("Swag Labs");
 
 
