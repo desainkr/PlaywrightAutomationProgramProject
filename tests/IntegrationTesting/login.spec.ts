6fbd1ee6614ace3693fa4503319c002d518182ed
desainkr <desainkr@gmail.com>
2026-02-09 17:05:38 -0800

    added new folder

---
+++ tests/IntegrationTesting/login.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+import { BASE_URL, USERNAME, PASSWORD } from './utils/envConfig.ts';
+test('Login to saucedemo application with valid cdentials', async ({ page }) => {
+    const loginpage = new LoginPage(page);
+    await page.goto(BASE_URL);
+    await loginpage.login(USERNAME, PASSWORD);
+    await expect(page.getByText('Swag Labs', { exact: true })).toBeVisible();
+    //await expect(page).toHaveURL(/inventory/);
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    await expect(page).toHaveTitle("Swag Labs");
+
+
+})
